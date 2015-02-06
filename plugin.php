5486fe82e3cf1146d01200bf63af54bb72f31f12
Alan Ng <jenfoong@gmail.com>
2015-02-06 12:37:37 +0800

    Add pootle updater

--- plugin.php
+++ plugin.php
@@ -14,6 +14,22 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+require_once(dirname(__FILE__) . '/classes/class-pootlepress-updater.php');
+
+add_action('init', 'pp_sfx_amr_updater');
+function pp_sfx_amr_updater()
+{
+	if (!function_exists('get_plugin_data')) {
+		include(ABSPATH . 'wp-admin/includes/plugin.php');
+	}
+	$data = get_plugin_data(__FILE__);
+	$wptuts_plugin_current_version = $data['Version'];
+	$wptuts_plugin_remote_path = 'http://www.pootlepress.com/?updater=1';
+	$wptuts_plugin_slug = plugin_basename(__FILE__);
+	new Pootlepress_Updater ($wptuts_plugin_current_version, $wptuts_plugin_remote_path, $wptuts_plugin_slug);
+}
+
+
 /**
  * Returns the main instance of SFX_Align_Menu_Right to prevent the need to use globals.
  *
